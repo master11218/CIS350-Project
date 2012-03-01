79ff4e5ff5479af505ee69fee606133078d1c126
David Xu <david@david-ThinkPad-X220.(none)>
2012-03-01 04:27:18 -0500

    playing around with maps

--- src/edu/upenn/cis350/MapProviderActivity.java
+++ src/edu/upenn/cis350/MapProviderActivity.java
@@ -1,13 +1,14 @@
 package edu.upenn.cis350;
 import android.app.Activity;
+import com.google.android.maps.MapActivity;
 import android.os.Bundle;
 
 
-public class MapProviderActivity extends Activity{
+public class MapProviderActivity extends MapActivity{
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.map);
         
         
     }
@@ -16,6 +17,12 @@ public class MapProviderActivity extends Activity{
 	public void onResume(){
 		super.onResume();
         
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
     
 }
