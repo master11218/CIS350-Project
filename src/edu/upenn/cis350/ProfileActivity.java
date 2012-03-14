2b6b8366344514d3cb1b05ec93f45e55e8f63efe
Tanvir Ahmed <aniantor@gmail.com>
2012-03-14 03:46:12 -0400

    added the profile stuff

--- src/edu/upenn/cis350/ProfileActivity.java
+++ src/edu/upenn/cis350/ProfileActivity.java
@@ -2,15 +2,93 @@ package edu.upenn.cis350;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 public class ProfileActivity extends Activity{
+	private User _userToLoad;
+	private Button save;
+	
+	private String gender;
+	private String phone;
+	private String address;
+	private String email;
+	private String name;
+	
+	private EditText nameField;
+	private EditText emailField;
+	private EditText addressField;
+	private EditText phoneField;
+	private RadioGroup genderField;
+	
+	private Context ownContext = this;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
         
-        
+        _userToLoad = (User)getIntent().getSerializableExtra("user");
+        if(_userToLoad == null){
+        	setContentView(R.layout.profile_new);
+        	save = (Button)this.findViewById(R.id.profile_new_save);
+        	
+        	nameField = (EditText)this.findViewById(R.id.profile_new_name);
+        	emailField = (EditText)this.findViewById(R.id.profile_new_email);
+        	addressField = (EditText)this.findViewById(R.id.profile_new_address);
+        	phoneField = (EditText)this.findViewById(R.id.profile_new_phone);
+        	genderField = (RadioGroup)this.findViewById(R.id.profile_new_genderGroup);
+        	
+        	save.setOnClickListener(new OnClickListener(){
+
+				public void onClick(View v) {
+					SharedPreferences settings = getSharedPreferences("UserData", 0);
+					SharedPreferences.Editor editor = settings.edit();
+					
+					name = nameField.getText().toString();
+					email = emailField.getText().toString();
+					address = addressField.getText().toString();
+					phone = phoneField.getText().toString();
+					if(genderField.getCheckedRadioButtonId() == R.id.profile_new_male)
+		        		gender = "Male";
+		        	else
+		        		gender = "Female";
+					
+					editor.putString("Id", "1");
+					editor.putString("Name", name);
+					editor.putString("Address", address);
+					editor.putString("Email", email);
+					editor.putString("Phone", phone);
+					editor.putString("Gender", gender);
+					editor.commit();
+					
+					Toast.makeText(ownContext, "Your information has been saved", Toast.LENGTH_SHORT).show();
+					finish();
+				}
+        	});
+        } else {
+        	setContentView(R.layout.profile_existing);
+        	TextView name = (TextView)this.findViewById(R.id.profile_existing_name);
+        	name.setText(_userToLoad.getName());
+        	
+        	TextView email = (TextView)this.findViewById(R.id.profile_existing_email);
+        	email.setText(_userToLoad.getEmail());
+        	
+        	TextView phone = (TextView)this.findViewById(R.id.profile_existing_phone);
+        	phone.setText(_userToLoad.getPhone());
+        	
+        	TextView address = (TextView)this.findViewById(R.id.profile_existing_address);
+        	address.setText(_userToLoad.getAddress());
+        	
+        	TextView gender = (TextView)this.findViewById(R.id.profile_existing_gender);
+        	gender.setText(_userToLoad.getGender());
+        }
     }
 
 	@Override
--- src/edu/upenn/cis350/VoiceActivity.java
+++ src/edu/upenn/cis350/VoiceActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.content.SharedPreferences;
 
 public class VoiceActivity extends Activity {
     /** Called when the activity is first created. */
@@ -16,19 +17,33 @@ public class VoiceActivity extends Activity {
     private Button m_button_map;
     private Button m_button_contact;
     private final Context m_context = this;
-    
+    private User currentUser;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        
     }
 
 	@Override
 	public void onResume(){
 		super.onResume();
+		
+        SharedPreferences userData = getSharedPreferences("UserData", 0);
+        if(!userData.contains("Id")){
+        	currentUser = null;
+        } else {
+        	Long id = Long.parseLong(userData.getString("Id", "-1"));
+        	String name = userData.getString("Name", "Not found");
+        	String address = userData.getString("Address", "Not found");
+        	String email = userData.getString("Email", "Not found");
+        	String phone = userData.getString("Phone", "Not found");
+        	String gender = userData.getString("Gender", "N/A");
+        	currentUser = new User(id, name, email, address, gender, phone);
+        }
+        
+        
+        System.out.println(currentUser);
         
         m_button_search = (Button)this.findViewById(R.id.button_search);
         m_button_profile= (Button)this.findViewById(R.id.button_profile);
@@ -47,6 +62,7 @@ public class VoiceActivity extends Activity {
 			
 			public void onClick(View v) {
 	        	Intent intent = new Intent(m_context, ProfileActivity.class);
+	        	intent.putExtra("user", currentUser);
 				startActivity(intent);
 			}
         });
