d0c2c1a94c662943686ff7c17264ce0baf248420
David Xu <david@david-ThinkPad-X220.(none)>
2012-03-14 03:48:53 -0400

    attempting map stuff

--- src/edu/upenn/cis350/MapProviderActivity.java
+++ src/edu/upenn/cis350/MapProviderActivity.java
@@ -16,16 +16,58 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MapProviderActivity extends MapActivity{
 	private MapView _myMapView;
 	private MapController _myMapController;
-	private ArrayList<Provider> _providers;
+	private ArrayList<Provider> _providers = new ArrayList<Provider>();;
 	private Double m_lat;
 	private Double m_long;
 	private Context m_context = this;
 
+	//For temporary shit
+	
+
+	public Provider generateProvider(String name, double latitude, double longitude){
+		Rating first = new Rating(3,1,new Date(System.currentTimeMillis()), "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+				5);
+		Rating second = new Rating(4,1,new Date(System.currentTimeMillis()), " adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehend",
+				3);
+		Rating third = new Rating(5,1,new Date(System.currentTimeMillis()), " This guy is awesome!!!!!!!!!!!!!!!!!!!!!!!",
+				3);
+		Rating fourth= new Rating(5,1,new Date(System.currentTimeMillis()), " I don't speak latin );",
+				4);
+		ArrayList<Rating> ratings = new ArrayList<Rating>();
+		
+		ratings.add(first);
+		ratings.add(second);
+		ratings.add(third);
+		ratings.add(fourth);
+		
+		//initialize a dummy provider.
+		return new Provider(1, name, "3400 Spruce Street, 8 Ravdin, Philadelphia, PA", "(215)662-3228", ratings, latitude, longitude);
+	}
+	
+	public void generateProviderList(){
+
+        //Generate a bunch of temporary random providers
+        _providers.add(generateProvider("Loraine Zachery", 39.951481, -75.200987));
+        _providers.add(generateProvider("Tyrone Bolan", 39.952481, -75.200987));
+        _providers.add(generateProvider("Roslyn Chico", 39.953481, -75.200987));
+        _providers.add(generateProvider("Sharron Becher", 39.954481, -75.200987));
+        _providers.add(generateProvider("Steidl Zachery", 39.955481, -75.200987));
+        _providers.add(generateProvider("Hugh Tandy", 39.956481, -75.200987));
+        _providers.add(generateProvider("Mathew Dimas", 39.957481, -75.200987));
+        _providers.add(generateProvider("Milagros Siegmund", 39.958481, -75.200987));
+        _providers.add(generateProvider("Serena Champine", 39.959481, -75.200987));
+        _providers.add(generateProvider("Allie Lunday", 39.950481, -75.200987));
+        
+	}
+	
+	
+	//actual code
 
 	MyLocation m_location = new MyLocation();
 	private void locationClick() {
@@ -53,8 +95,9 @@ public class MapProviderActivity extends MapActivity{
 		//grab the location, set the latitude and longitude
 		locationClick();
 		
-		_providers = new ArrayList<Provider>();
-		_providers.add((Provider)getIntent().getSerializableExtra("providers"));
+		Provider intentProvider = (Provider)getIntent().getSerializableExtra("providers");
+		if(intentProvider != null)
+			_providers.add(intentProvider);
 		if(_providers.size() == 1)
 			System.out.println("Would have shown directions");
 
