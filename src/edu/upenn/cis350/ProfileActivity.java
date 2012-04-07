e515ea7b74160d59dc63c28c23d2248fb9dfde4e
Darren Xu <darrenxu90@gmail.com>
2012-04-07 18:29:25 -0400

    Refactored some more

--- src/edu/upenn/cis350/ProfileActivity.java
+++ src/edu/upenn/cis350/ProfileActivity.java
@@ -14,6 +14,11 @@ import android.widget.Toast;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/**
+ * Contains information about the user
+ * @author DXU
+ *
+ */
 public class ProfileActivity extends Activity{
 	private User _userToLoad;
 	private Button save;
@@ -32,12 +37,17 @@ public class ProfileActivity extends Activity{
 	
 	private Context ownContext = this;
 	
-	@Override
+	/**
+	 * Initialize the user's profile on creation
+	 */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         _userToLoad = (User)getIntent().getSerializableExtra("user");
+        
+        //test to see whether this user already has a profile
         if(_userToLoad == null){
+        	//this user does  not have a profile, set view to profile_new_save
         	setContentView(R.layout.profile_new);
         	save = (Button)this.findViewById(R.id.profile_new_save);
         	
@@ -47,63 +57,15 @@ public class ProfileActivity extends Activity{
         	phoneField = (EditText)this.findViewById(R.id.profile_new_phone);
         	genderField = (RadioGroup)this.findViewById(R.id.profile_new_genderGroup);
         	
+        	//clicking this button saves the information
         	save.setOnClickListener(new OnClickListener(){
 
 				public void onClick(View v) {
-					//User info is set in the shared preferences.
-					SharedPreferences settings = getSharedPreferences("UserData", 0);
-					SharedPreferences.Editor editor = settings.edit();
-					
-					//get the user's information
-					name = nameField.getText().toString();
-					email = emailField.getText().toString();
-					address = addressField.getText().toString();
-					phone = phoneField.getText().toString();
-					if(genderField.getCheckedRadioButtonId() == R.id.profile_new_male)
-		        		gender = "Male";
-		        	else
-		        		gender = "Female";
-					
-					//get the user's id via the http request, and store it in the database
-					HttpRequest http = new HttpRequest();
-					String uri = "http://spectrackulo.us/350/register.php?name=" + name + 
-							"&address=" + address + "&gender=" + gender + "&email=" + email + "&phone=" + phone;
-					
-					System.out.println(uri);
-					String id = http.execHttpRequest(uri, HttpRequest.HttpMethod.Get, "");
-					System.out.println(id);
-					
-					
-					//store the information on the device
-					editor.putString("Id", id);
-					editor.putString("Name", name);
-					editor.putString("Address", address);
-					editor.putString("Email", email);
-					editor.putString("Phone", phone);
-					editor.putString("Gender", gender);
-					editor.commit();
-					
-					//let the user know that their information has been saved
-					Toast.makeText(ownContext, "Your information has been saved", Toast.LENGTH_SHORT).show();
-					finish();
+					saveProfile();
 				} 
         	});
         } else {
-        	setContentView(R.layout.profile_existing);
-        	TextView name = (TextView)this.findViewById(R.id.profile_existing_name);
-        	name.setText(_userToLoad.getName());
-        	
-        	TextView email = (TextView)this.findViewById(R.id.profile_existing_email);
-        	email.setText(_userToLoad.getEmail());
-        	
-        	TextView phone = (TextView)this.findViewById(R.id.profile_existing_phone);
-        	phone.setText(_userToLoad.getPhone());
-        	
-        	TextView address = (TextView)this.findViewById(R.id.profile_existing_address);
-        	address.setText(_userToLoad.getAddress());
-        	
-        	TextView gender = (TextView)this.findViewById(R.id.profile_existing_gender);
-        	gender.setText(_userToLoad.getGender());
+        	loadProfile();
         }
     }
 
@@ -111,6 +73,64 @@ public class ProfileActivity extends Activity{
 	public void onResume(){
 		super.onResume();
         
+	}
+	
+	public void saveProfile() {
+		//User info is set in the shared preferences.
+		SharedPreferences settings = getSharedPreferences("UserData", 0);
+		SharedPreferences.Editor editor = settings.edit();
+		
+		//get the user's information
+		name = nameField.getText().toString();
+		email = emailField.getText().toString();
+		address = addressField.getText().toString();
+		phone = phoneField.getText().toString();
+		if(genderField.getCheckedRadioButtonId() == R.id.profile_new_male)
+    		gender = "Male";
+    	else
+    		gender = "Female";
+		
+		//get the user's id via the http request, and store it in the database
+		HttpRequest http = new HttpRequest();
+		String uri = "http://spectrackulo.us/350/register.php?name=" + name + 
+				"&address=" + address + "&gender=" + gender + "&email=" + email + "&phone=" + phone;
+		
+		
+		String id = http.execHttpRequest(uri, HttpRequest.HttpMethod.Get, "");
+		
+		
+		
+		//store the information on the device
+		editor.putString("Id", id);
+		editor.putString("Name", name);
+		editor.putString("Address", address);
+		editor.putString("Email", email);
+		editor.putString("Phone", phone);
+		editor.putString("Gender", gender);
+		editor.commit();
+		
+		//let the user know that their information has been saved
+		Toast.makeText(ownContext, "Your information has been saved", Toast.LENGTH_SHORT).show();
+		finish();
+	}
+	
+	public void loadProfile() {
+		//this user already has a profile, set view to profile_existing
+    	setContentView(R.layout.profile_existing);
+    	TextView name = (TextView)this.findViewById(R.id.profile_existing_name);
+    	name.setText(_userToLoad.getName());
+    	
+    	TextView email = (TextView)this.findViewById(R.id.profile_existing_email);
+    	email.setText(_userToLoad.getEmail());
+    	
+    	TextView phone = (TextView)this.findViewById(R.id.profile_existing_phone);
+    	phone.setText(_userToLoad.getPhone());
+    	
+    	TextView address = (TextView)this.findViewById(R.id.profile_existing_address);
+    	address.setText(_userToLoad.getAddress());
+    	
+    	TextView gender = (TextView)this.findViewById(R.id.profile_existing_gender);
+    	gender.setText(_userToLoad.getGender());
 	}
     
 }
