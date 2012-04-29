54b18e9fe78e8507a0de8253036798f73e2816e0
unknown <henryou@henryou-PC.(none)>
2012-04-29 14:26:18 -0400

    fix the provider search bug, start changing rating instance

--- src/edu/upenn/cis350/entities/Rating.java
+++ src/edu/upenn/cis350/entities/Rating.java
@@ -10,15 +10,31 @@ public class Rating implements Serializable{
 	private String date;
 	private String review;
 	private Integer rating;
+	private Integer communication_rating = 0;
+	private Integer office_environment_rating = 0;
+	private Integer friendliness_rating = 0;
 	
-	public Rating(long user, long provider, String date, String desc, int rate){
-		user_id = user;
-		provider_id = provider;
+	public Rating(long user, long provider, String date, String desc, int rate,
+			int communication_rating, int office_environment_rating,
+			int friendliness_rating) {
+		this.user_id = user;
+		this.provider_id = provider;
 		this.date = date;
-		review = desc;
-		rating = rate;
+		this.review = desc;
+		this.rating = rate;
+		this.communication_rating = communication_rating;
+		this.office_environment_rating = office_environment_rating;
+		this.friendliness_rating = friendliness_rating;
 	}
 	
+	public Rating(long user, long provider, String date, String desc, int rate) {
+		this.user_id = user;
+		this.provider_id = provider;
+		this.date = date;
+		this.review = desc;
+		this.rating = rate;
+	}
+
 	public Long getUser(){
 		return user_id;
 	}
@@ -33,5 +49,17 @@ public class Rating implements Serializable{
 	}
 	public Integer getRating(){
 		return rating;
+	}
+	
+	public Integer getCommunication_rating() {
+		return communication_rating;
+	}
+
+	public Integer getOffice_environment_rating() {
+		return office_environment_rating;
+	}
+
+	public Integer getFriendliness_rating() {
+		return friendliness_rating;
 	}
 }
